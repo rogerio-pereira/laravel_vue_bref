295a762d5e43b3168ea0d983b66faf5251c6c527
Rogerio Pereira <hi@rogeriopereira.dev>
2024-05-13 12:15:12 -0400

    feat: Install axios on vue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,10 +5,21 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+//Axios Config
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = import.meta.VITE_BACKEND_URL
+axios.defaults.headers.common['Access-Control-Origin'] = '*';
+axios.defaults.headers.common['Accept'] = 'application/json';
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+// axios.defaults.headers.common['X-Authorization'] = '';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+    .use(VueAxios, axios)
 
 app.mount('#app')
