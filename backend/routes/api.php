08e3604ff16a0247c60db782d5551e5fc719a775
Rogerio Pereira <hi@rogeriopereira.dev>
2024-05-13 15:21:15 -0400

    feat(backend): Add route group with Auth:sanctum middleware

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
