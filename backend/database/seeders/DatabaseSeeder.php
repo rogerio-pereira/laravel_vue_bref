772fb0aa052aef288be31854ac066656d20051be
Rogerio Pereira <hi@rogeriopereira.dev>
2024-05-14 10:47:42 -0400

    feat(backend): Add User Seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'User Tes',
+            'email' => 'test@email.com',
+            'password' => bcrypt('test'),
         ]);
     }
 }
