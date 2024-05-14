340143ae18f42a7dfde8360d4d6e6d4faede364e
Rogerio Pereira <hi@rogeriopereira.dev>
2024-05-14 15:51:14 -0400

    refactor(backend): Refactor Picture Accessor to allow return null

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -45,6 +47,16 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Get the user's first name.
+     */
+    protected function picture(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string|null $value) => $value ? Storage::url($value) : null ,
+        );
     }
 
     public function deleteTokens() : void 
