ff41b7a1f22e4c9c66506dd733400802dbd41314
Rogerio Pereira <hi@rogeriopereira.dev>
2024-05-22 11:50:14 -0400

    feat(frontend): Add axios interceptor to get XSRF token

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -13,10 +13,36 @@ import vuetify from '@/plugins/vuetify'
 axios.defaults.withXSRFToken = true
 axios.defaults.withCredentials = true
 axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL
-axios.defaults.headers.common['Access-Control-Origin'] = '*';
+axios.defaults.headers.common['Access-Control-Origin'] = import.meta.env.VITE_URL;
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = import.meta.env.VITE_URL;
 axios.defaults.headers.common['Accept'] = 'application/json';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 // axios.defaults.headers.common['X-Authorization'] = '';
+
+/**
+ * https://github.com/axios/axios/issues/6047
+ */
+//Define XSRF token
+axios.interceptors.response.use((config) => {
+    const cookie = getCookie('XSRF-TOKEN');
+    const token = decodeURIComponent(cookie);
+
+    axios.defaults.headers.common['X-XSRF-TOKEN'] = token;  
+    return config;
+});
+// Utility function to retrieve a cookie value by name
+function getCookie(name) {
+    const cookies = document.cookie.split(';')
+    for (const cookie of cookies) {
+        const [key, value] = cookie.split('=')
+
+        if (key.trim() === name) {
+            return value
+        }
+    }
+
+    return ''
+}
 
 const app = createApp(App)
 
