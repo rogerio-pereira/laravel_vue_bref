7c61a68eec4170657fed70f8f60a4a6ef418c046
Rogerio Pereira <hi@rogeriopereira.dev>
2024-05-29 17:13:49 -0400

    refactor(backend): Add items to be returned on route '/'

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
+use App\Models\RandomSchedule;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return [
+        'Laravel' => app()->version(),
+        'database' => config('database.default'),
+        // 'config/s3' => config('filesystems.disks.s3'),
+        RandomSchedule::limit(10)
+                ->get(),
+        'files' => Storage::allFiles('/'),
+    ];
 });
 
 require __DIR__.'/auth.php';
